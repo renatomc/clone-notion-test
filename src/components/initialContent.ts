1baa5790d521496fdde11942ab0abc86de36b510
Renato Martinez Costa <renatomc@hotmail.com>
2023-07-27 12:07:56 -0300

    Clonando o Notion

---
+++ src/components/initialContent.ts
@@ -0,0 +1,4 @@
+export const initialContent = `<h1>Meu Notion</h1>
+                              <p>Notion do Renato</p>
+                              <pre><code class="language-javascript">console.log('Ola mundo')</code></pre>
+                              `
